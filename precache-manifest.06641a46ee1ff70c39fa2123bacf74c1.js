f65c64a23720f4f8288aeb047a8accc9d642c630
SupreethSudhakaranMenon <supreethsudhakaranmenon14@gmail.com>
2021-07-20 20:59:16 +0530

    Updates

---
+++ precache-manifest.06641a46ee1ff70c39fa2123bacf74c1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85c7b58a71db85ae6440e0c198dda8af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc92f01dea1badf6b7ef",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "ea4c23db1757a4f0b504",
+    "url": "/static/js/2.c6a95f53.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.c6a95f53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc92f01dea1badf6b7ef",
+    "url": "/static/js/main.1358a632.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "64696921969365d69d5ec6b0156d0f05",
+    "url": "/static/media/SupreethSKaran.64696921.jpg"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "3a3247b8d87dfd404f8810128982a900",
+    "url": "/static/media/resume.3a3247b8.pdf"
+  }
+]);
